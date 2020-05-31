0e00d60cb23e0f30961a0771ac62694a22254777
scottlecomte <54564921+scottlecomte@users.noreply.github.com>
2020-05-31 10:59:20 -0700

    Update volume-control.ino

--- esp8266 code/volume-control.ino
+++ esp8266 code/volume-control.ino
@@ -1,1 +1,99 @@
+//* This code is for a line level Wi-Fi volume controller to be used with Home Assiatant *//
 
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these variables.
+
+const char* ssid = "........"; // Your SSID 
+const char* password = "........"; // Your Wi-Fi password
+const char* mqtt_server = "........"; // Your MQTT broker
+const char* mqtt_topic = "audio/volume"; // You can leave this topic or change if you would like If you change, you will need to update your Home Assistant automation topic
+const char* mqtt_client_id = "Volume-controller"; // Name for this device on your MQTT network
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Change volume and cycle LED - this is where logic goes to change based on mqtt data
+  
+  if ((char)payload[0] > 50) {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = mqtt_client_id;
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      client.subscribe(mqtt_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output for testing
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
+}
